a90578a2af8641da6caa2741dc50fc23191abb1c
Frank Pierce <31361830+FrankPierce@users.noreply.github.com>
2017-08-26 00:10:09 -0500

    Update WeGottaToken.sol

--- WeGottaToken.sol
+++ WeGottaToken.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.13;
 
+contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
+
 contract SafeMath {
   //internals
 
@@ -25,11 +27,12 @@ contract SafeMath {
   }
 }
 
-contract WeGotta {
+contract WeGotta{
     string public name; 
     string public symbol; 
     uint8 public decimals;
     uint256 public totalSupply;
+    address public owner;
     
     /* This creates an array with all balances */
     mapping (address => uint256) public balanceOf;
@@ -38,22 +41,55 @@ contract WeGotta {
     /* This generates a public event on the blockchain that will notify clients */
     event Transfer(address indexed from, address indexed to, uint256 value);
     
-    function WeGotta(uint256 initialSupply) {
-      name = "WeGotta"
-      symbol = "WGT"
-      balanceOf[msg.sender] = initialSupply;  /*set to be anywhere between 15 to 50 million as of right now it gives the creator all initial tokens */
-      
-  } 
-   /* Send coins */
-      function transfer(address _to, uint256 _value) {
-          
-          /*Check if sender has balance and for overflow */
-          require(balanceOf[msg.sender] >= _value && balanceOf[-to] + _value >= balanceOf[_to]);
-          
-          /* Add and subtract new balances */
-          balanceOf[msg.sender] -= _value;
-          balanceOf[_to] += _value;
-          
-      }
-     
+    /* This notifies clients about the amount burnt */
+    event Burn(address indexed from, uint256 value);
+   
+   /* Initializes contract with initial supply tokens to the creator of the contract */
+    function WeGottaToken is StandardToken, SafeMath {
+      name = "WeGotta";
+      symbol = "WGT";
+      decimals = 8;
+      balanceOf[msg.sender] = 50000000; /*Set initial supply to 50 million*/
+      totalSupply = 50000000;
+   } 
+   
+   /* Internal transfer, only can be called by this contract */
+    function _transfer(address _from, address _to, uint _value) internal {
+      require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
+      require (balanceOf[_from] > _value);                // Check if the sender has enough
+      require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
+      require(!frozenAccount[_from]);                     // Check if sender is frozen
+      require(!frozenAccount[_to]);                       // Check if recipient is frozen
+      balanceOf[_from] -= _value;                         // Subtract from the sender
+      balanceOf[_to] += _value;                           // Add the same to the recipient
+      Transfer(_from, _to, _value);
+   }  
+   
+   function transfer(address _to, uint256 _value) {
+      _transfer(msg.sender, _to, _value)
+   }
+   
+   /* Allow another contract or person to spend some tokens in your behalf */
+   function approve(address _spender, uint256 _value)
+      returns (bool success) {
+      allowance[msg.sender][_spender] = _value;
+      return true;
+   }
+   
+   /* A contract or  person attempts to get the tokens of somebody else.
+    *  This is only allowed if the token holder approved. */
+   function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+      require (_value < allowance[_from][msg.sender]);       // Check allowance
+      allowance[_from][msg.sender] -= _value;
+      _transfer(_from, _to, _value);
+      return true;
+   }
+   
+   function burn(uint256 _value) returns (bool success) {
+      require (balanceOf[msg.sender] > _value);             //  Check if the sender has enough
+      balanceOf[msg.sender] -= _value;                      // Subtract from the sender
+      totalSupply -= _value;                                // Updates totalSupply
+      Burn(msg.sender, _value);
+      return true;
+  }
 }
