1d28fe9aacb749f4941e618d0b3c7b12fc13ac2c
Frank Pierce <31361830+FrankPierce@users.noreply.github.com>
2017-08-27 15:49:45 -0500

    Update WeGottaToken.sol

--- WeGottaToken.sol
+++ WeGottaToken.sol
@@ -97,7 +97,7 @@ contract WeGotta {
     /// @notice Remove `_value` tokens from the system irreversibly
     /// @param _value the amount of money to burn
     function burn(uint256 _value) returns (bool success) {
-        require (balanceOf[msg.sender] > _value);            // Check if the sender has enough
+        require (balanceOf[msg.sender] > _value);             // Check if the sender has enough
         balanceOf[msg.sender] -= _value;                      // Subtract from the sender
         totalSupply -= _value;                                // Updates totalSupply
         Burn(msg.sender, _value);
